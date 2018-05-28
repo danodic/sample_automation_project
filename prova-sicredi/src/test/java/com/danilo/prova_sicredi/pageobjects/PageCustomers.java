89a76cc6c052d1d9a018564093e115a217591cbb
Danilo Guimaraes <danilo.guimaraes@outlook.com.br>
2018-05-27 23:36:24 -0300

    Organized imports.

--- prova-sicredi/src/test/java/com/danilo/prova_sicredi/pageobjects/PageCustomers.java
+++ prova-sicredi/src/test/java/com/danilo/prova_sicredi/pageobjects/PageCustomers.java
@@ -5,7 +5,6 @@ import java.util.Date;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
